daf27ddd5a64fe6d9133c1f81d9a7132b8dcf590
SivaSuji <sujinisivakumar@gmail.com>
2017-11-26 13:37:21 -0500

    First Commit
    Uploading all Files

---
+++ GitPractice/src/test/java/PracticeToUseGit/Sample2.java
@@ -0,0 +1,14 @@
+package PracticeToUseGit;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		
+		String name = "Susan";
+		int age = 25;
+
+		System.out.println("I am " + name + " and I am " + age + " years old."); 
+		
+	}
+}	
+
